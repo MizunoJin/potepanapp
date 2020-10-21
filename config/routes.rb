043cff4be2ae21cee9ae124a3ef68de04caf0e88
Ubuntu <ubuntu@ip-172-31-24-96.ap-northeast-1.compute.internal>
2020-10-21 20:54:58 +0000

    サインアップページの作成、簡単なユーザー情報ページの作成、ユーザー登録機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get '/about', to: 'top_page#about'
   get '/contact', to: 'top_page#contact'
   get '/signup', to: 'users#new'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
+  
 end
