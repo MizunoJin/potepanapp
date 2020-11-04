e4a1b1ab95ae89112c39e701d6be50b02b30385f
Ubuntu <ubuntu@ip-172-31-24-96.ap-northeast-1.compute.internal>
2020-11-04 19:10:40 +0000

    turn off validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,8 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                       uniqueness: true
   has_secure_password
-  validates :password, presence: true, on: :create, length: {minimum: 6}, allow_nil: true
+  #validates :password, presence: true, on: :create, length: {minimum: 6}, allow_nil: true
+  validates :password, presence: false, on: :facebook_login
   
     # 渡された文字列のハッシュ値を返す
   def User.digest(string)
@@ -106,7 +107,7 @@ class User < ApplicationRecord
        user = User.new
     end
     user.uid   = auth.uid
-    user.username  = auth.info.name
+    user.name  = auth.info.name
     user.email = auth.info.email
     user.oauth_token = auth.credentials.token
     user.oauth_expires_at = Time.at(auth.credentials.expires_at)
